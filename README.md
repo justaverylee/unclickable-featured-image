804a71b5dfb0ac3a8003607d193130348bec3760
Zach Gottesman <git@zachgottesman.com>
2019-06-25 21:26:16 -0700

    Added Code+Readme
    Added version 1.0 code and starts of a REAME

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # unclickable-featured-image-wp
 A wordpress plugin that forces all featured images to NOT be links
+
+If your theme makes thumbnail pictures links (which casuses a useless refresh on click), 
+use this plugin to disable that. That's it. There is no options menu, or settings.
+Once enabled, it will make every featured image (on a post/page) unclickable.
+
+Installation:
+- Download this repository
+- Upload the 'unclickable-featured-image' folder to your webserver
+- Place the 'unclickable-featured-image' folder in the '/wp-content/plugins' folder
+- Activate the plugin from the Wordpress admin console
---
+++ unclickable-featured-image/unclickable-featured-image.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Unclickable Featured Image
+ * Plugin URI: https://github.com/zggz/unclickable-featured-image
+ * Description: A wordpress plugin that forces all featured images to NOT be links
+ * Version: 1.0
+ * Auther: Zach Gottesman
+ * Author URI: http://zachgottesman.com
+ */
+ 
+ // Wait until the theme tries to get the thumbnail
+ add_filter('post_thumbnail_html', 'unlink_thumbnail', 1, 5);
+ 
+ /**
+  * Disables the link from a featured image (if it exists)
+  * This is accomplished by wrapping the image in an <a> tag with no action
+  * Typically $html is given as just a <img> tag pointing to the thumbnail
+  * @param string $html
+  * @param int $post_id
+  * @param int $post_thumbnail_id
+  * @param string $size
+  * @param array $attr
+  * @return string
+  */
+ function unlink_thumbnail( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
+ 	if (is_singular()) {
+		$html = "<a href='javascript:void(0);'>$html</a>";
+	}
+ 	return $html;
+ }
